717a767637e5be63076a9f79297e056550205333
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-09-15 06:50:30 +0900

    05_Badコードの実装1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,59 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DDD.WinForm.Models;
+using System.Data;
+using Microsoft.Data.Sqlite;
 
 namespace DDD.WinForm.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly string ConnectionString = @"Data Source=/Users/shiratorinaoki/DataBase/sqlite/Udemy-DDD-Part1.db";
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int areaId = 1)
     {
+        string sql_ = @"SELECT * FROM Weather WHERE AreaId = @AreaId ORDER BY DataDate DESC LIMIT 1";
+
+        DataTable dt = new DataTable();
+        try
+        {
+            using (var connection = new SqliteConnection(ConnectionString))
+            using (var command = new SqliteCommand(sql_, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@AreaId", areaId);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        ViewData["datadate"] = reader["datadate"]?.ToString() ?? "データなし";
+                        ViewData["condition"] = reader["Condition"]?.ToString() ?? "データなし";
+                        ViewData["temperature"] = reader["Temperature"]?.ToString() ?? "データなし";
+                    }
+                    else
+                    {
+                        ViewData["datadate"] = "データなし";
+                        ViewData["condition"] = "データなし";
+                        ViewData["temperature"] = "データなし";
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "データベース接続エラー");
+            ViewData["condition"] = "接続エラー";
+            ViewData["condition"] = "接続エラー";
+            ViewData["temperature"] = "接続エラー";
+        }
+
         return View();
     }
 
