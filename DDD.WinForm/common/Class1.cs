f1ff73c077607d0270ff412ff135389fd5ef13c6
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-09-16 21:43:38 +0900

    -フォルダ構造修正

---
+++ DDD.WinForm/common/Class1.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DDD.WinForm.common;
+
+public class Class1
+{
+    public static int Add(int v1, int v2)
+    {
+        return 0;
+    }
+}
---
+++ DDDTest.Tests/Test1.cs
@@ -0,0 +1,14 @@
+﻿using DDD.WinForm.common;
+
+namespace DDDTest.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var val = Class1.Add(1, 2);
+        Assert.AreEqual(3, val);
+    }
+}
