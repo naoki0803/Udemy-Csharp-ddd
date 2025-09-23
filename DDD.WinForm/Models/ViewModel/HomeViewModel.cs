ef5809b0d9c20401637e7912aad6b0383447cbc4
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-09-24 05:55:49 +0900

    HomeViewModelクラスを新規作成し、天気データの取得とプロパティの設定を行うSearchメソッドを実装。テスト用にHomeViewModelTestクラスを追加し、WeatherMockを用いたユニットテストを実施。MSTest関連のパッケージを更新。

---
+++ DDD.WinForm/Models/ViewModel/HomeViewModel.cs
@@ -0,0 +1,51 @@
+
+using System;
+using System.Data;
+using DDD.WinForm.Common;
+using DDD.WinForm.Repositories;
+using Microsoft.AspNetCore.Routing.Template;
+
+namespace DDD.WinForm.Models;
+
+public class HomeViewModel
+{
+    private IWeatherRepository _Weather;
+
+    public HomeViewModel(IWeatherRepository Weather)
+    {
+        this._Weather = Weather;
+    }
+
+    public string? AreaId { get; set; } = string.Empty;
+    public string? DataDate { get; set; } = string.Empty;
+    public string? Condition { get; set; } = string.Empty;
+    public string? Temperature { get; set; } = string.Empty;
+
+    public HomeViewModel Search(string? areaId)
+    {
+        var dt = _Weather.GetLatest(Convert.ToInt32(areaId));
+
+        if (dt.Rows.Count > 0)
+        {
+            var row = dt.Rows[0];
+
+            AreaId = areaId;
+            DataDate = row["DataDate"]?.ToString() ?? "データなし";
+            Condition = row["Condition"]?.ToString() ?? "データなし";
+            Temperature = CommonFunc.RoundString(Convert.ToSingle(row["Temperature"]?.ToString()),
+                            CommonConst.TemperatureDecimalPoint)
+                            + " "
+                            + CommonConst.TemperatureUnitName
+                            ?? "データなし";
+        }
+        else
+        {
+            AreaId = areaId;
+            DataDate = "データなし";
+            Condition = "データなし";
+            Temperature = "データなし";
+        }
+
+        return this;
+    }
+}
---
+++ DDDTest.Tests/HomeViewModelTest.cs
@@ -0,0 +1,53 @@
+using System.Data;
+using DDD.WinForm.Models;
+using DDD.WinForm.Repositories;
+
+namespace DDDTest.Tests;
+
+[TestClass]
+public class HomeViewModelTest
+{
+    [TestMethod]
+    public void シナリオ()
+    {
+        var viewModel = new HomeViewModel(new WeatherMock());
+        {
+            Assert.AreEqual("", viewModel.AreaId);
+            Assert.AreEqual("", viewModel.DataDate);
+            Assert.AreEqual("", viewModel.Condition);
+            Assert.AreEqual("", viewModel.Temperature);
+
+            viewModel.AreaId = "2";
+            viewModel.Search(viewModel.AreaId);
+
+            Assert.AreEqual("2", viewModel.AreaId);
+            Assert.AreEqual("2025/09/22 15:22:33", viewModel.DataDate);
+            Assert.AreEqual("5", viewModel.Condition);
+            Assert.AreEqual("33.80 ℃", viewModel.Temperature);
+        }
+    }
+
+    internal class WeatherMock : IWeatherRepository
+    {
+
+        public DataTable GetLatest(int areaId)
+        {
+            var dt = new DataTable();
+
+            dt.Columns.Add("AreaId", typeof(int));
+            dt.Columns.Add("DataDate", typeof(DateTime));
+            dt.Columns.Add("Condition", typeof(int));
+            dt.Columns.Add("Temperature", typeof(float));
+
+            var newRow = dt.NewRow();
+            newRow["AreaId"] = 2;
+            newRow["DataDate"] = Convert.ToDateTime("2025/09/22 15:22:33");
+            newRow["Condition"] = 5;
+            newRow["Temperature"] = 33.8f;
+
+            dt.Rows.Add(newRow);
+            return dt;
+            // throw new NotImplementedException();
+        }
+    }
+}
