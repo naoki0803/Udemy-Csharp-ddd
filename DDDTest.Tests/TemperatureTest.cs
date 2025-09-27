ff0202fb5c2b7b4523a2165266b40e63f6fa2c7c
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-09-27 20:48:07 +0900

    Temperatureクラスのユニットテストを新規作成し、小数点以下2桁での温度表示機能を検証。

---
+++ DDDTest.Tests/TemperatureTest.cs
@@ -0,0 +1,16 @@
+using DDD.Domain.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Company.TestProject1;
+
+[TestClass]
+public class TemperatureTest
+{
+    [TestMethod]
+    public void 小数点以下2桁でまるめて表示できる()
+    {
+        var t = new Temperature(12.3f);
+        Assert.AreEqual(12.3f, t.Value);
+        Assert.AreEqual("12.30 ℃", t.DisplayValue);
+    }
+}
