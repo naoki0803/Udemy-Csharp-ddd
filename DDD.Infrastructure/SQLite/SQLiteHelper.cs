a0c248b9c147cec3fcb37e13d0088b7f1af4d318
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-10-10 05:50:30 +0900

    AreaRepositoryをリファクタリングし、SQLiteHelperを使用したデータ取得によりGetDataメソッドを簡素化。データベース操作を改善するため、SQLiteHelperにジェネリックメソッドQueryを追加。

--- DDD.Infrastructure/SQLite/SQLiteHelper.cs
+++ DDD.Infrastructure/SQLite/SQLiteHelper.cs
@@ -3,4 +3,29 @@ namespace DDD.Infrastructure.SQLite;
 public static class SQLiteHelper
 {
     public const string ConnectionString = @"Data Source=/Users/shiratorinaoki/DataBase/sqlite/Udemy-DDD-Part1.db";
-}
+
+    internal static IReadOnlyList<T> Query<T>(string sql, Func<SqliteDataReader, T> createEntity)
+    {
+        var results = new List<T>();
+        try
+        {
+            using (var connection = new SqliteConnection(ConnectionString))
+            using (var command = new SqliteCommand(sql, connection))
+            {
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        results.Add(createEntity(reader));
+                    }
+                }
+                return results.AsReadOnly();
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException("データベース接続エラー", ex);
+        }
+    }
