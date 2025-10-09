7814e696cfe298c881f2283ba11faf5ef248b0ac
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-10-10 06:02:34 +0900

    featch-pullテスト

--- DDD.WinForm/Controllers/HomeController.cs
+++ DDD.WinForm/Controllers/HomeController.cs
@@ -28,6 +28,10 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Test()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
